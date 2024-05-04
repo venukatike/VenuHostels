8475a24a3a1fe2d8ecc2cdc1ac1cc9fbfdd4ca43
--global <--global>
2024-05-04 23:57:33 +0530

    added admin page

--- SearchHostels/src/app/app-routing.module.ts
+++ SearchHostels/src/app/app-routing.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+// import { HomeComponent } from './home/home.component';
+// import { AboutComponent } from './about/about.component';
+// import { ContactComponent } from './contact/contact.component';
+import { AdminLoginComponent } from './components/admin-login/admin-login.component';
 
 const routes: Routes = [
   {path:'',component:HomeComponent},
+  { path: 'home', component: HomeComponent },
+ // { path: 'about', component: AboutComponent },
+ // { path: 'contact', component: ContactComponent },
+  { path: 'admin', component: AdminLoginComponent },
+ // { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
 
 @NgModule({
--- SearchHostels/src/app/app.module.ts
+++ SearchHostels/src/app/app.module.ts
@@ -3,18 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UserLoginComponent } from './components/user-login/user-login.component';
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { AdminLoginComponent } from './components/admin-login/admin-login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserLoginComponent,
     HomeComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    AdminLoginComponent
   ],
   imports: [
     BrowserModule,
